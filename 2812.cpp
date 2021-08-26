741526abcb020643c98a9669c234afff728f838a
JaeKyungHeo <jaekyung507@naver.com>
2021-08-27 01:53:55 +0900

    백준 2812번 풀이(그리디 알고리즘)

---
+++ 2812.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N, K, i = 0;
+	string num;
+	vector<char>v;
+	cin >> N >> K;
+	cin >> num;
+
+	while (i != num.size()) {
+		while (K != 0 && !v.empty() && v.back() < num[i]) {
+			v.pop_back();
+			K--;
+		}
+		v.push_back(num[i]);
+		i++;
+	}
+	
+	//아직 못 빼준것이 있다면
+	while (K--)
+		v.pop_back();
+	
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i];
+
+	return 0;
+}
