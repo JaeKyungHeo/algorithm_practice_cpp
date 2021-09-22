bfe8937c4181bf73be92ff619e2d206e39016f43
JaeKyungHeo <jaekyung507@naver.com>
2021-09-22 13:04:24 +0900

    12100_2048수정2(삼성SW기출)

--- 삼성SW기출(12100번).cpp
+++ 삼성SW기출(12100번).cpp
@@ -15,21 +15,21 @@ int N;
 int board[21][21];
 int M = 0;
 
-void solution(int n, int m) {
-	if (n == 0) {
-		if (m > M)
-			M = m;
+void solution(int n) {
+	if (n == 0)
 		return;
-	}
+
 	int sub[21][21];
 	vector<int>a;
 	vector<int>b;
-	//위
-	//copy
+	
+	//copy(백트래킹을 위해 사용)
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++)
 			sub[i][j] = board[i][j];
 	}
+
+	//위
 	//1. 한쪽으로 밀기
 	//2. 0이 아닌 숫자만 추출
 	//3. 연속된 두 숫자를 비교하며 같은 수라면 앞쪽으로 합침(뒤에 수는 0으로 갱신) 
@@ -40,21 +40,25 @@ void solution(int n, int m) {
 			if (board[j][i] > 0)
 				a.push_back(board[j][i]);
 		}
-		for (int j = 1; j < a.size(); j++) {
-			if (a[j - 1] == a[j]) {
+		for (int j = 0; j < a.size(); j++) {
+			if (j == 0 && a.size() == 1)//1. a의 크기가 1인 경우
+				b.push_back(a[j]);
+			else if (j == 0 && a.size() != 1)//2. 이후 과정이 0이 아닌 부분에서의 진행을 위해 j=0 pass
+				continue;
+			else if (a[j - 1] == a[j]) {//3. 같은 수들의 연속되면 합치고 앞쪽에 배치
 				b.push_back(2 * a[j - 1]);
-				if (m < 2 * a[j - 1]) 
-					m = 2 * a[j - 1];
+				if (M < 2 * a[j - 1]) 
+					M = 2 * a[j - 1];
 				a[j] = 0;
 			}
-			else if(a[j-1] != 0)
+			else if(a[j - 1] != 0)//4. 앞 수와 같지 않으면서 0이 아니라면 앞에 수는 b벡터에 넣음
 				b.push_back(a[j - 1]);
-			//마지막 부분까지 확인
-			if (j == a.size() - 1 && a[j] > 0) {
+			//5. j가 0이 아니면서 마지막 부분 처리
+			if (j != 0 && j == a.size() - 1 && a[j] > 0)
 				b.push_back(a[j]);
-			}
 		}
 
+		//원래 board 채우기
 		for (int j = 0; j < b.size(); j++)
 			board[j][i] = b[j];
 		for (int j = b.size(); j < N; j++)
@@ -62,14 +66,14 @@ void solution(int n, int m) {
 		a.clear();
 		b.clear();
 	}
-	solution(n - 1, m);
+
+	solution(n - 1);
+
 	//복원
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++)
 			board[i][j] = sub[i][j];
 	}
-
-
 
 	//아래
 	for (int i = 0; i < N; i++) {
@@ -77,19 +81,22 @@ void solution(int n, int m) {
 			if (board[j][i] > 0)
 				a.push_back(board[j][i]);//a벡터에는 0이 아닌 정수가 들어감
 		}
-		for (int j = 1; j < a.size(); j++) {
-			if (a[j - 1] == a[j]) {
+		for (int j = 0; j < a.size(); j++) {
+			if (j == 0 && a.size() == 1)
+				b.push_back(a[j]);
+			else if (j == 0 && a.size() != 1)
+				continue;
+			else if (a[j - 1] == a[j]) {
 				b.push_back(2 * a[j - 1]);
-				if (m < 2 * a[j - 1])
-					m = 2 * a[j - 1];
+				if (M < 2 * a[j - 1])
+					M = 2 * a[j - 1];
 				a[j] = 0;
 			}
 			else if (a[j - 1] != 0)//인접한 같은 수를 합쳐준 뒤 다시한번 0이 아닌 수를 b벡터에 담아줌
 				b.push_back(a[j - 1]);
 			//마지막 부분까지 확인
-			if (j == a.size() - 1 && a[j] > 0) {
+			if (j != 0 && j == a.size() - 1 && a[j] > 0)
 				b.push_back(a[j]);
-			}
 		}
 
 		for (int j = 0; j < b.size(); j++)
@@ -100,7 +107,8 @@ void solution(int n, int m) {
 		b.clear();
 	}
 	
-	solution(n - 1,m);
+	solution(n - 1);
+
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++)
 			board[i][j] = sub[i][j];
@@ -112,19 +120,22 @@ void solution(int n, int m) {
 			if (board[i][j] > 0)
 				a.push_back(board[i][j]);
 		}
-		for (int j = 1; j < a.size(); j++) {
-			if (a[j - 1] == a[j]) {
+		for (int j = 0; j < a.size(); j++) {
+			if (j == 0 && a.size() == 1)
+				b.push_back(a[j]);
+			else if (j == 0 && a.size() != 1)
+				continue;
+			else if (a[j - 1] == a[j]) {
 				b.push_back(2 * a[j - 1]);
-				if (m < 2 * a[j - 1])
-					m = 2 * a[j - 1];
+				if (M < 2 * a[j - 1])
+					M = 2 * a[j - 1];
 				a[j] = 0;
 			}
 			else if (a[j - 1] != 0)
 				b.push_back(a[j - 1]);
 			//마지막 부분까지 확인
-			if (j == a.size() - 1 && a[j] > 0) {
+			if (j != 0 && j == a.size() - 1 && a[j] > 0)
 				b.push_back(a[j]);
-			}
 		}
 
 		for (int j = 0; j < b.size(); j++)
@@ -135,7 +146,7 @@ void solution(int n, int m) {
 		b.clear();
 	}
 
-	solution(n - 1, m);
+	solution(n - 1);
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++)
 			board[i][j] = sub[i][j];
@@ -147,19 +158,22 @@ void solution(int n, int m) {
 			if (board[i][j] > 0)
 				a.push_back(board[i][j]);
 		}
-		for (int j = 1; j < a.size(); j++) {
-			if (a[j - 1] == a[j]) {
+		for (int j = 0; j < a.size(); j++) {
+			if (j == 0 && a.size() == 1)
+				b.push_back(a[j]);
+			else if (j == 0 && a.size() != 1)
+				continue;
+			else if (a[j - 1] == a[j]) {
 				b.push_back(2 * a[j - 1]);
-				if (m < 2 * a[j - 1])
-					m = 2 * a[j - 1];
+				if (M < 2 * a[j - 1])
+					M = 2 * a[j - 1];
 				a[j] = 0;
 			}
 			else if (a[j - 1] != 0)
 				b.push_back(a[j - 1]);
 			//마지막 부분까지 확인
-			if (j == a.size() - 1 && a[j] > 0) {
+			if (j != 0 && j == a.size() - 1 && a[j] > 0)
 				b.push_back(a[j]);
-			}
 		}
 
 		for (int j = 0; j < b.size(); j++)
@@ -170,25 +184,25 @@ void solution(int n, int m) {
 		b.clear();
 	}
 
-	solution(n - 1,m);
+	solution(n - 1);
 
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
-	int max = 0;
 	cin >> N;
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cin >> board[i][j];
-			if (max < board[i][j])
-				max = board[i][j];
+			if (M < board[i][j])
+				M = board[i][j];
 		}
 	}
 
-	solution(5, max);
+	solution(5);
+
 	cout << M;
 
 	return 0;
